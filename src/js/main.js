518727fb8d5ba74effdd632a3db6dda9f7ba8298
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-04-10 17:57:21 +0300

    paintings-arts

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+import modals from './modules/modals';
+import sliders from './modules/sliders';
+import forms from './modules/forms';
+import maskPhone from './modules/maskPhone';
+import checkTextInputs from './modules/checkTextInputs';
+import showMorestyles from './modules/showMorestyles';
+import calc from './modules/calc';
+import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
+import scrolling from './modules/scrolling';
+import drop from './modules/drop';
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    maskPhone();
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMorestyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
+    pictureSize('.sizes-block');
+    accordion('.accordion-heading');
+    burger('.burger-menu', '.burger');
+    scrolling('.pageup');
+    drop();
+});
